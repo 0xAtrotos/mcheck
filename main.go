c605607b8cc69f24af7bd719c639e31d0ee0c897
George Violaris <violarisgeorge@gmail.com>
2014-04-17 17:46:48 +0300

    First Test

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func check() {
+
+	f, err := os.Open("config.txt")
+
+	if err != nil {
+		fmt.Println("Error opening configuration file ", err)
+		os.Exit(1)
+	}
+
+	defer f.Close()
+
+	r := bufio.NewReaderSize(f, 4*1024)
+
+	line, isPrefix, err := r.ReadLine()
+	for err == nil && !isPrefix {
+		s := string(line)
+		fmt.Println()
+		fmt.Println(s)
+		line, isPrefix, err = r.ReadLine()
+
+		check, err := os.Stat(s)
+		if err != nil {
+			panic(err)
+		}
+		since := check.ModTime()
+		fmt.Println(since)
+		fmt.Println()
+	}
+
+}
+
+func checkPath(file string) {
+
+	check, err := os.Stat(file)
+	if err != nil {
+		panic(err)
+	}
+	since := check.ModTime()
+
+	fmt.Println(since)
+
+}
+
+func main() {
+
+	if len(os.Args[0]) != 0 {
+		checkPath(os.Args[0])
+	} else {
+		check()
+	}
+
+}
